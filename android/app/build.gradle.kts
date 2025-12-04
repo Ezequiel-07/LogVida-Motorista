31a19bb6d7612b9d4725b1c792af37ac63bdf2e6
ezequielantunes07 <ezequielantunes07@gmail.com>
2025-12-04 09:01:21 +0000

    Initial commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -45,7 +48,7 @@ android {
 
 // Add the desugaring dependency
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 
--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,14 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:location/location.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<void> addLocation(LocationData locationData) async {
-    await _db.collection('locations').add({
-      'latitude': locationData.latitude,
-      'longitude': locationData.longitude,
-      'timestamp': FieldValue.serverTimestamp(),
-    });
+    final user = _auth.currentUser;
+    if (user != null) {
+      await _db.collection('driverLocations').doc(user.uid).set({
+        'latitude': locationData.latitude,
+        'longitude': locationData.longitude,
+        'accuracy': locationData.accuracy,
+        'heading': locationData.heading,
+        'speed': locationData.speed,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+    }
   }
 }
